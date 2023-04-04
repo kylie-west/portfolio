b964972eafcd7ba0644b4ec253811ee42cd53fde
Kylie West <kmichellewest@gmail.com>
2023-04-03 18:06:24 -0700

    Update content + close overlay by clicking outside image + text hover effect

--- index.js
+++ index.js
@@ -7,74 +7,79 @@ const projectImages = document.querySelectorAll(".project-image");
 /* --- Functions --- */
 
 async function getProfile() {
-	const response = await fetch("https://api.github.com/users/kylie-west");
-	const data = await response.json();
-	return { bio: data.bio, avatar: data.avatar_url };
+  const response = await fetch("https://api.github.com/users/kylie-west");
+  const data = await response.json();
+  return { bio: data.bio, avatar: data.avatar_url };
 }
 
 function hideNavbar() {
-	navbar.style.transform = "translateY(-100px)";
+  navbar.style.transform = "translateY(-100px)";
 }
 
 function showNavbar() {
-	navbar.style.transform = "translateY(0)";
+  navbar.style.transform = "translateY(0)";
 }
 
 function viewImage(e) {
-	if (window.innerWidth <= 800) return;
+  if (window.innerWidth <= 800) return;
 
-	const overlay = document.createElement("div");
-	const closeButton = document.createElement("div");
-	const image = e.target.cloneNode();
+  const overlay = document.createElement("div");
+  const closeButton = document.createElement("div");
+  const image = e.target.cloneNode();
 
-	overlay.classList.add("overlay");
-	closeButton.classList.add("close-button");
-	image.classList.add("enlarged-image");
+  overlay.classList.add("overlay");
+  closeButton.classList.add("close-button");
+  image.classList.add("enlarged-image");
 
-	overlay.appendChild(closeButton);
-	overlay.appendChild(image);
+  overlay.appendChild(closeButton);
+  overlay.appendChild(image);
 
-	closeButton.addEventListener("click", () => overlay.remove());
-	document.body.prepend(overlay);
+  document.addEventListener("click", (e) => {
+    if (e.target === overlay) {
+      overlay.remove();
+    }
+  });
+  closeButton.addEventListener("click", () => overlay.remove());
+  document.body.prepend(overlay);
 }
 
 /* --- Setting up elements and event listeners --- */
 
 // Fetch and insert data from GitHub
-getProfile().then(profile => {
-	document.getElementById("bio").innerText = profile.bio;
-	document.getElementById("profile-photo").src = profile.avatar;
+getProfile().then((profile) => {
+  document.getElementById("bio").innerText = profile.bio;
+  document.getElementById("profile-photo").src = profile.avatar;
 });
 
 // Scroll to section on navlink click
-navlinks.forEach(link => {
-	link.addEventListener("click", e => {
-		e.preventDefault();
-		const hash = link.hash;
-		const target = document.querySelector(hash);
-		target.scrollIntoView({
-			behavior: "smooth",
-			alignToTop: true
-		});
-		if (hash !== "#header") {
-			setTimeout(hideNavbar, 700);
-		}
-	});
+navlinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const hash = link.hash;
+    const target = document.querySelector(hash);
+    target.scrollIntoView({
+      behavior: "smooth",
+      alignToTop: true,
+    });
+    if (hash !== "#header") {
+      setTimeout(hideNavbar, 700);
+    }
+  });
 });
 
 // Hide navbar on scroll down
 let prevScrollPosition = window.pageYOffset;
-window.addEventListener("scroll", e => {
-	const currentScrollPosition = window.pageYOffset;
+window.addEventListener("scroll", (e) => {
+  const currentScrollPosition = window.pageYOffset;
 
-	if (currentScrollPosition > prevScrollPosition) {
-		hideNavbar();
-	} else {
-		showNavbar();
-	}
+  if (currentScrollPosition > prevScrollPosition) {
+    hideNavbar();
+  } else {
+    showNavbar();
+  }
 
-	prevScrollPosition = currentScrollPosition;
+  prevScrollPosition = currentScrollPosition;
 });
 
 // Enlarge project images on click
-projectImages.forEach(image => image.addEventListener("click", viewImage));
+projectImages.forEach((image) => image.addEventListener("click", viewImage));
